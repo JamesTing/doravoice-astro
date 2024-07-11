f140b937424b6faaab6ee4f1499c90b3da26a181
JamesTing <ahshengchen@gmail.com>
2024-07-11 17:31:31 +0800

    首页思维导图功能介绍内容丰富、修订

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://amymind.com',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), icon()],
   i18n: {
     defaultLocale: "en",
     locales: ["zh-cn", "en"],
